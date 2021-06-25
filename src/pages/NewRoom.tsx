31fd8cf2df1c877b6e0efe708989fb1d6fa60599
Camila Nepomuceno <camila.cno@gmail.com>
2021-06-25 01:34:40 -0300

    New room page

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,30 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+
+import { Button } from '../components/Button';
+import '../styles/auth.scss';
+
+export function NewRoom() {
+  return (
+    <div id='page-auth'>
+      <aside>
+        <img src={illustrationImg} alt='Perguntas e respostas' />
+        <strong>Toda pergunta tem uma resposta.</strong>
+        <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
+      </aside>
+      <main>
+        <div className='main-content'>
+          <img src={logoImg} alt='Letmeask' />
+          <h2>Criar uma nova sala</h2>
+          <form>
+            <input type='text' placeholder='Nome da sala' />
+            <Button type='submit'>Criar sala </Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <a href='#'>Clique aqui</a>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
